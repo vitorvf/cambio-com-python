9f15994d051f3e5b4ce8eba1706729bc4f8ac8f1
vitor ferreira <vitorferreira@gmail.com>
2019-01-28 20:48:37 -0300

    atualização das funções

--- dollar.py
+++ dollar.py
@@ -1,15 +1,16 @@
 import requests
 import json
+import pandas as pd
 
 def main():
-    cambio_dollar(None)
-    cambio_euro(None)
+    dollar = cambio_dollar()
+    euro = cambio_euro()
+    print(euro)
+    exportar_csv(dollar, euro)
 
-def cambio_dollar(url):
+def cambio_dollar(url = "http://data.fixer.io/api/latest?access_key=65c2c5494f5a965219e5a0d3568300e4&format=1"):
     #Se o usuário não passar nenhuma URL, vai entrar no IF e será atribuida
     #a variável URL o endereço abaixo
-    if url is None:
-        url = "http://data.fixer.io/api/latest?access_key=65c2c5494f5a965219e5a0d3568300e4&format=1"
     response = requests.get(url)
     if response.status_code == 200:
         print("Conseguiu se conectar...")
@@ -17,13 +18,12 @@ def cambio_dollar(url):
         taxa_usd = dados['rates']['USD']
         taxa_brl = dados['rates']['BRL']
         real = taxa_brl/taxa_usd
-        print("O dólar está custando %.2f reais " % real)
+        real = round(real, 2)
+        return real
     else:
         print("Site com algum problema!")
 
-def cambio_euro(url):
-    if url is None:
-        url = "http://data.fixer.io/api/latest?access_key=65c2c5494f5a965219e5a0d3568300e4&format=1"
+def cambio_euro(url = "http://data.fixer.io/api/latest?access_key=65c2c5494f5a965219e5a0d3568300e4&format=1"):
     response = requests.get(url)
     if response.status_code == 200:
         print("Conseguiu se conectar...")
@@ -31,9 +31,18 @@ def cambio_euro(url):
         taxa_eur = dados['rates']['EUR']
         taxa_brl = dados['rates']['BRL']
         real = taxa_brl/taxa_eur
-        print("O euro está custando %.2f reais " % real)
+        real = round(real, 2)
+        print(real)
+        return real
     else:
         print("Site com algum problema!")
+
+def exportar_csv(dollar, euro):
+    print(euro)
+    linha = {'Dollar - USD': [dollar], 'Euro - EUR': [euro]}
+    frame = pd.DataFrame(linha, columns = ['Dollar - USD', 'Euro - EUR'])
+    frame.to_csv('mo.csv')
+    print('dados salvo na tabela!')
 
 #Pede para que o programa execute a nossa função main
 if __name__ == '__main__':
